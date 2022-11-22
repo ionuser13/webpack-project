8da95b41cc4ca8efb4305971acb44a05d8127e6e
userjohn <johnchacpi479@gmail.com>
2022-11-21 19:44:02 -0500

    added css loaders and stylus

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const path = require("path");;
-
+const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
     entry: "./src/index.js",
     output: {
@@ -18,6 +19,14 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.css|.styl$/i,
+                use: [
+                    miniCssExtractPlugin.loader, 
+                    "css-loader", 
+                    "stylus-loader"
+                ]
             }
         ]
     },
@@ -26,6 +35,7 @@ module.exports = {
             inject: true,
             template: "./public/index.html",
             filename: "./index.html"
-        })
+        }),
+        new miniCssExtractPlugin(),
     ]
 }
